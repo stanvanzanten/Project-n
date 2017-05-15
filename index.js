7cba1255c3d8922cd723ce335acd827fc4dad8db
Stan van Zanten <stanzanten@hotmail.com>
2017-05-15 19:04:40 +0200

    j

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook/', function (req, res) {
 			}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			sendTextMessage(sender, text.substring(0, 200), token)
 			continue
 		}
 	}
