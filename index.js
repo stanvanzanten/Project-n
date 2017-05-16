2876e9195fef8fa30386e6c2c1801af8570f0191
Stan van Zanten <stanzanten@hotmail.com>
2017-05-16 17:15:25 +0200

    g

--- index.js
+++ index.js
@@ -65,6 +65,9 @@ app.post('/webhook/', function (req, res) {
 			let text = JSON.stringify(event.postback)
 			//sendTextMessage(sender, "Je bestelling is ontvangen! Als je via de onderstaande link betaalt sturen we ze direct naar je toe.")
 			sendGeneric2Message(sender)
+			/*if (res.sendStatus(200) = true){ 
+			sendGeneric3Message(sender)
+			}*/
 			continue
 		}
 		
