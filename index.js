cc473d02eee3a283f21709a896ed78c773f817c9
Stan van Zanten <stanzanten@hotmail.com>
2017-06-09 12:09:12 +0200

    d

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/webhook/', function (req, res) {
 				continue
 			}
 			if (text.includes("flow festival")) {
-				sendTextMessage(sender, "Leuk dat je naar het flow festival wil! Wat voor kaartjes zou je willen?")
+				sendTextMessage(sender, "Leuk dat je naar het flow festival wil {{first_name}}! Wat voor kaartjes zou je willen?")
 				sendGenericMessage(sender)
 			}
 			else if (text.includes("kaart")) {
