a8545359024950ab01a00e0fc35a2156fe4aa4e8
Stan van Zanten <stanzanten@hotmail.com>
2017-06-09 11:28:35 +0200

    d

--- index.js
+++ index.js
@@ -302,12 +302,12 @@ function sendQuickReply(sender) {
 				"text": "Pick a color:",
 				"quick_replies": [
 					{
-						"type": "text",
+						"type": "postback",
 						"title": "Red",
 						"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
 					},
 					{
-						"type": "text",
+						"type": "postback",
 						"title": "Green",
 						"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
 					}
