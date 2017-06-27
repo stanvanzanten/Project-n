8ed7b970096d9155dc9bb6dce783c93ef1d20b7a
Stan van Zanten <stanzanten@hotmail.com>
2017-06-27 19:07:18 +0200

    d

--- index.js
+++ index.js
@@ -115,7 +115,6 @@ app.post('/webhook/', function (req, res) {
 					break;
 
 				case 'Prijzen':
-					sendTextMessage(sender, "De prijzen van VIP en Regular kaartjes verschillen, kijk zelf maar eens: https://dmjzka.dm2302.livefilestore.com/y4m5HHipfWrrQ92uGgbwE7Z7ImK4ZADTMIwqu6yxsl0YuJQ_84sMaONzRGlVL8K6E2BKfbzWU_VQWBCzLKPbEuBRP242D3V29KmJ8b3Km1i6EQlZ9IqiLFV1thQwabO1ekSjGFAp1GSi1XaOHbwa9CEZA6S5_eVZHptA6BvrRe0nqO7G4sV3HjjUbzQUFycRDqX7Ds6RECQU-0lHbJK-7C02A?width=154&height=219&cropmode=none")
 					sendImage2Message(sender)
 					break;
 			}
@@ -255,11 +254,11 @@ function sendImage2Message(sender) {
 
 				"elements": [{
 
-					"title": "Waar kan ik je mee helpen?",
+					"title": "De prijzen van de kaarten verschillen natuurlijk!",
 
-					"subtitle": "Ik kan je met verschillende dingen helpen!",
+					"subtitle": "Kijk zelf maar eens!",
 
-					"image_url": "https://www.visitljubljana.com/assets/gallery/flow.jpg"
+					"image_url": "https://onedrive.live.com/embed?cid=90027EF9547C8BCA&resid=90027EF9547C8BCA%2119248&authkey=AEKhFMLR1rePOBg"
 
 				}]
 
